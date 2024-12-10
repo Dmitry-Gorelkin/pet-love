e3f28bf450aa354d385f953c2fcddc170d249a2e
Дмитрий Горелкин <gorelkind@gmail.com>
2024-12-10 14:59:38 +0200

    add

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,13 @@
 const HomePage = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Take good care of your small pets</h1>
+      <p>
+        Choosing a pet for your home is a choice that is meant to enrich your life with immeasurable
+        joy and tenderness.
+      </p>
+    </>
+  );
 };
 
 export default HomePage;
