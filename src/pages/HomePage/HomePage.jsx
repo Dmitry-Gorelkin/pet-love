a2c307dccb68098a5a4ee0d347773e8c8ee351f0
Дмитрий Горелкин <gorelkind@gmail.com>
2024-12-19 10:29:56 +0200

    add_img

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,4 @@
-import img from '../../assets/homePage/homePage_d.jpg';
-import img2x from '../../assets/homePage/homePage_d@2x.jpg';
-import imgTablet from '../../assets/homePage/homePage_t.jpg';
-import imgTablet2x from '../../assets/homePage/homePage_t@2x.jpg';
-import imgMobile from '../../assets/homePage/homePage_m.jpg';
-import imgMobile2x from '../../assets/homePage/homePage_m@2x.jpg';
+import { HomePageImg } from './HomePage.styled';
 
 const HomePage = () => {
   return (
@@ -13,17 +8,7 @@ const HomePage = () => {
         Choosing a pet for your home is a choice that is meant to enrich your life with immeasurable
         joy and tenderness.
       </p>
-
-      {/* <div>
-        <img src={img_d} alt="dog and woman" />
-      </div> */}
-      <picture>
-        <source srcSet={`${imgMobile} 1x, ${imgMobile2x} 2x`} media="(min-width:375px)" />
-        <source srcSet={`${imgTablet} 1x, ${imgTablet2x} 2x`} media="(min-width:768px)" />
-        <source srcSet={`${img} 1x, ${img2x} 2x`} media="(min-width:1216px)" />
-
-        <img src={imgMobile2x} alt="Парень в очках" />
-      </picture>
+      <HomePageImg />
     </>
   );
 };
--- src/pages/HomePage/HomePage.styled.jsx
+++ src/pages/HomePage/HomePage.styled.jsx
@@ -1,38 +1,56 @@
 import styled from 'styled-components';
+import imgMob1x from '../../assets/homePage/homePage_m.jpg';
+import imgMob2x from '../../assets/homePage/homePage_m@2x.jpg';
+import imgTab1x from '../../assets/homePage/homePage_t.jpg';
+import imgTab2x from '../../assets/homePage/homePage_t@2x.jpg';
+import img1x from '../../assets/homePage/homePage_d.jpg';
+import img2x from '../../assets/homePage/homePage_d@2x.jpg';
 
-export const HomePagePicture = styled.picture`
+export const HomePageImg = styled.div`
   display: block;
-  border-radius: 60px;
-`;
 
-export const HomePageSourceMobile = styled.source`
+  width: 100%;
+  height: 402px;
+
+  background-image: url(${imgMob1x});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+
+  border-radius: 30px;
+
+  /* High-resolution mobile */
+  @media only screen and (max-width: 767px) {
+    background-image: url(${imgMob2x});
+  }
+
+  /* Tablet */
   @media screen and (min-width: 768px) {
-    display: none;
+    height: 496px;
+
+    background-image: url(${imgTab1x});
+
+    border-radius: 60px;
   }
 
+  /* High-resolution tablet */
+  @media screen and (min-width: 768px) and (min-resolution: 192dpi) {
+    height: 496px;
+
+    background-image: url(${imgTab2x});
+
+    border-radius: 60px;
+  }
+
+  /* Desktop */
   @media screen and (min-width: 1280px) {
-  }
-`;
-
-export const HomePageSourceTable = styled.source`
-  display: none;
-
-  @media screen and (min-width: 768px) {
-    display: block;
+    height: 384px;
+    background-image: url(${img1x});
   }
 
-  @media screen and (min-width: 1280px) {
-    display: none;
-  }
-`;
-
-export const HomePageSource = styled.source`
-  display: none;
-
-  @media screen and (min-width: 768px) {
-  }
-
-  @media screen and (min-width: 1280px) {
-    display: block;
+  /* High-resolution desktop */
+  @media screen and (min-width: 1280px) and (min-resolution: 192dpi) {
+    height: 384px;
+    background-image: url(${img2x});
   }
 `;
